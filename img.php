17dc0d6fcafe501a39db60b656f440d816ebbf2d
SergioGG <45605296+sergiogongil@users.noreply.github.com>
2019-08-12 17:28:05 +0200

    Add files via upload

--- img.php
+++ img.php
@@ -4,14 +4,20 @@ class imagen {
    public function __construct(){
           if(isset($_GET["x"])){ $this->ancho = $_GET["x"]; }else{ $this->ancho = 600; }
           if(isset($_GET["y"])){ $this->altmax = $_GET["y"]; }else{ $this->altmax = 400; }
-          if(isset($_GET["n"])){ $this->nombre = $_GET["n"]; }else{ $this->nombre = "default.jpg"; }
-          if(!is_file($this->nombre)){ $this->nombre = "default.jpg"; }
+          if(isset($_GET["n"])){ $this->nombre = $_GET["n"]; }else{ $this->nombre = ""; }
+          if(!file_exists($this->nombre)){ $this->nombre = "default.jpg"; }
    }
    
-   public function imageJpg(){
+   public function mostrar(){
           $datos = getimagesize($this->nombre);
-
-          if($datos[2]==2){ $img = @imagecreatefromjpeg($this->nombre); }
+          $formato = $datos["mime"];
+                if($formato == "image/jpg"){        $img = @imagecreatefromjpeg($this->nombre);
+                }elseif($formato == "image/png"){   $img = @imagecreatefrompng($this->nombre);
+                }elseif($formato == "image/gif"){   $img = @imagecreatefromgif($this->nombre);
+                }elseif($formato == "image/bmp"){   $img = @imagecreatefrombmp($this->nombre);
+                }elseif($formato == "image/jpeg"){  $img = @imagecreatefromjpeg($this->nombre);
+                }else{                              $img = @imagecreatefromjpeg($this->nombre); }
+          
              $ratio = ($datos[0] / $this->ancho);
              $this->altmax = ($datos[1] / $ratio);
 
@@ -23,13 +29,18 @@ class imagen {
 
 $thumb = imagecreatetruecolor($this->ancho,$this->altmax);
 imagecopyresampled($thumb, $img, 0, 0, 0, 0, $this->ancho, $this->altmax, $datos[0], $datos[1]);
-header("Content-type: image/jpeg");
-imagejpeg($thumb);
+header("Content-type: $formato");
+if($formato == "image/jpg"){          imagejpeg($thumb);
+}elseif($formato == "image/png"){     imagepng($thumb);
+}elseif($formato == "image/gif"){     imagegif($thumb);
+}elseif($formato == "image/bmp"){     imagebmp($thumb);
+}elseif($formato == "image/jpeg"){    imagejpeg($thumb);
+}else{                                imagejpeg($thumb); }
 imagedestroy($thumb);
 }
 
  
 }
 $imagen = new imagen();
-$imagen -> imageJpg();
+$imagen -> mostrar();
 ?>
