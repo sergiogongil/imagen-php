148ca3cbb3ba7b811492cc2a3f0aa5c13c8279e3
SergioGG <45605296+sergiogongil@users.noreply.github.com>
2019-08-14 17:24:34 +0200

    Add files via upload

--- img.php
+++ img.php
@@ -1,13 +1,12 @@
 <?php /* https://github.com/sergiogongil - MIT License - Copyright (c) 2019 SergioGG */
 class imagen {
-
    public function __construct(){
-          if(isset($_GET["x"])){ if($_GET["x"]>1280){ $this->ancho = 1280; }else{ $this->ancho = $_GET["x"]; }}else{ $this->ancho = 600; }
-          if(isset($_GET["y"])){ if($_GET["y"]>1024){ $this->altmax = 1024; }else{ $this->altmax = $_GET["y"]; }}else{ $this->altmax = 400; }
+          if(isset($_GET["x"]) AND is_numeric($_GET["x"])){ if($_GET["x"]>1280){ $this->ancho = 1280; }else{ $this->ancho = $_GET["x"]; }}else{ $this->ancho = 600; }
+          if(isset($_GET["y"]) AND is_numeric($_GET["y"])){ if($_GET["y"]>1024){ $this->altmax = 1024; }else{ $this->altmax = $_GET["y"]; }}else{ $this->altmax = 400; }
           if(isset($_GET["n"])){ $this->nombre = strip_tags($_GET["n"]); }else{ $this->nombre = ""; }
           if(!file_exists($this->nombre)){ $this->nombre = "data/default.jpg"; }
    }
-   
+
    public function mostrar(){
           $datos = getimagesize($this->nombre);
           $formato = $datos["mime"];
@@ -30,16 +29,12 @@ class imagen {
 $thumb = imagecreatetruecolor($this->ancho,$this->altmax);
 imagecopyresampled($thumb, $img, 0, 0, 0, 0, $this->ancho, $this->altmax, $datos[0], $datos[1]);
 
-                           /* Texto en imagen */
-if(isset($_GET["t"])){ $t = strip_tags($_GET["t"]); imagestring($thumb, 3, 40, 20, $t, 0xFFBA00); }
+if(isset($_GET["t"])){  /* Texto en imagen */ $t = strip_tags($_GET["t"]); imagestring($thumb, 3, 40, 20, $t, 0xFFBA00); }
 
-if(isset($_GET["m"])){     /* Marca de agua */
-$estampa = imagecreatefrompng('data/estampa.png');
-imagecopy($thumb, $estampa, imagesx($thumb) - imagesx($estampa) - 25, imagesy($thumb) - imagesy($estampa) - 25, 0, 0, imagesx($estampa), imagesy($estampa));
-}
+if(isset($_GET["m"])){  /* Marca de agua */ $estampa = imagecreatefrompng('data/estampa.png');
+imagecopy($thumb, $estampa, imagesx($thumb) - imagesx($estampa) - 25, imagesy($thumb) - imagesy($estampa) - 25, 0, 0, imagesx($estampa), imagesy($estampa)); }
 
 header("Content-type: $formato");
-
 if($formato == "image/jpg"){          imagejpeg($thumb);
 }elseif($formato == "image/png"){     imagepng($thumb);
 }elseif($formato == "image/gif"){     imagegif($thumb);
@@ -48,12 +43,6 @@ if($formato == "image/jpg"){          imagejpeg($thumb);
 }else{                                imagejpeg($thumb); }
 imagedestroy($thumb);
 }
-
-
-
-
-
-
 
 }
 $imagen = new imagen();
