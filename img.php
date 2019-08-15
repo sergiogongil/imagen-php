48c96970d5e61b8db6a071baac67b9912c0f3769
SergioGG <45605296+sergiogongil@users.noreply.github.com>
2019-08-15 17:01:03 +0200

    Ajuste de proporciones
    Incorporado ajuste de proporciones de tamaño de la imagen a razón de su ancho y alto.

--- img.php
+++ img.php
@@ -1,6 +1,6 @@
 <?php /* https://github.com/sergiogongil - MIT License - Copyright (c) 2019 SergioGG */
 class imagen {
-   public function __construct(){
+   public function __construct(){                              /* Definidos un ancho y alto máximos */
           if(isset($_GET["x"]) AND is_numeric($_GET["x"])){ if($_GET["x"]>1280){ $this->ancho = 1280; }else{ $this->ancho = $_GET["x"]; }}else{ $this->ancho = 600; }
           if(isset($_GET["y"]) AND is_numeric($_GET["y"])){ if($_GET["y"]>1024){ $this->altmax = 1024; }else{ $this->altmax = $_GET["y"]; }}else{ $this->altmax = 400; }
           if(isset($_GET["n"])){ $this->nombre = strip_tags($_GET["n"]); }else{ $this->nombre = ""; }
@@ -8,8 +8,8 @@ class imagen {
    }
 
    public function mostrar(){
-          $datos = getimagesize($this->nombre);
-          $formato = $datos["mime"];
+          $datos = getimagesize($this->nombre);     /* Obtener el tamaño de una imagen */
+          $formato = $datos["mime"];                /* Crea una nueva imagen a partir de un fichero o de una URL */
                 if($formato == "image/jpg"){        $img = @imagecreatefromjpeg($this->nombre);
                 }elseif($formato == "image/png"){   $img = @imagecreatefrompng($this->nombre);
                 }elseif($formato == "image/gif"){   $img = @imagecreatefromgif($this->nombre);
@@ -17,31 +17,33 @@ class imagen {
                 }elseif($formato == "image/jpeg"){  $img = @imagecreatefromjpeg($this->nombre);
                 }else{                              $img = @imagecreatefromjpeg($this->nombre); }
 
-             $ratio = ($datos[0] / $this->ancho);
-             $altmax = ($datos[1] / $ratio);
+             /* Calculamos y ajustamos las proporciones de la imagen */
+          $ancho = $datos["0"]; $alto = $datos["1"];
+          $x_ratio = $this->ancho / $ancho;
+          $y_ratio = $this->altmax / $alto;
+          if( ($ancho <= $this->ancho) && ($alto <= $this->altmax) ){ $ancho_final = $ancho; $alto_final = $alto; }
+	      elseif (($x_ratio * $alto) < $this->altmax){ $alto_final = ceil($x_ratio * $alto); $ancho_final = $this->ancho; }
+	      else{ $ancho_final = ceil($y_ratio * $ancho); $alto_final = $this->altmax; }
 
-             if($altmax>$this->altmax){
-                $ancho=$this->altmax*$this->ancho/$this->altmax;
-                $this->altmax=$altmax;
-                $this->ancho=$ancho;
-             }
-
-$thumb = imagecreatetruecolor($this->ancho,$this->altmax);
-imagecopyresampled($thumb, $img, 0, 0, 0, 0, $this->ancho, $this->altmax, $datos[0], $datos[1]);
+         /* Crear una nueva imagen de color verdadero */
+         $thumb = imagecreatetruecolor($ancho_final,$alto_final);
+         /* Copia y cambia el tamaño de parte de una imagen redimensionándola */
+         imagecopyresampled($thumb, $img, 0, 0, 0, 0, $ancho_final, $alto_final, $datos[0], $datos[1]);
 
 if(isset($_GET["b"])){  /* Imagen en Blanco y Negro */ imagefilter($thumb, IMG_FILTER_GRAYSCALE); }
 if(isset($_GET["t"])){  /* Texto en imagen */ $t = strip_tags($_GET["t"]); imagestring($thumb, 3, 40, 20, $t, 0xFFBA00); }
 if(isset($_GET["m"])){  /* Marca de agua */ $estampa = imagecreatefrompng('data/estampa.png');
+/* Copiar parte de una imagen */
 imagecopy($thumb, $estampa, imagesx($thumb) - imagesx($estampa) - 25, imagesy($thumb) - imagesy($estampa) - 25, 0, 0, imagesx($estampa), imagesy($estampa)); }
-
-header("Content-type: $formato");
+/* Enviar encabezado */
+header("Content-type: $formato");   /* Exportar la imagen al navegador o a un fichero */
 if($formato == "image/jpg"){          imagejpeg($thumb);
 }elseif($formato == "image/png"){     imagepng($thumb);
 }elseif($formato == "image/gif"){     imagegif($thumb);
 }elseif($formato == "image/bmp"){     imagebmp($thumb);
 }elseif($formato == "image/jpeg"){    imagejpeg($thumb);
 }else{                                imagejpeg($thumb); }
-imagedestroy($thumb);
+imagedestroy($thumb); /* Destruir una imagen */
 }
 
 }
